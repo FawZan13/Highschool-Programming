ae8c7118e2b1bbd15045d1263f333562870c1ea8
FawZan13 <fawzanbinmokhtar@gmail.com>
2023-04-29 16:18:17 +0600

    first commit

---
+++ program02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("Fawzan Bin Mokhtar. ");
+    printf("Dhaka City College. ");
+    printf("Azimpur, Dhaka.");
+}
---
+++ program03.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("Enter the value of first number = ");
+    scanf("%d", &a);
+    printf("Enter the value of second number = ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The sum of the number is = %d", sum);
+}
